4511eef14e64f5a064ed1d111526c1a146c490cb
thneang <thneang@gmail.com>
2016-03-30 01:29:24 +0200

    client demande et acceptation connexion privé

--- src/fr/upem/net/tcp/protocol/Writters.java
+++ src/fr/upem/net/tcp/protocol/Writters.java
@@ -10,19 +10,10 @@ public class Writters {
 	
 	private static final Charset UTF8 = Charset.forName("utf-8");
 	
-	/** Attente active !!
-	 * 
-	 * @param sc
-	 * @param buff
-	 * @throws IOException
-	 */
-	public static void writeFully(SocketChannel sc,ByteBuffer buff) throws IOException{
-		
-		while(buff.hasRemaining()){
-			sc.write(buff);
-		}
-	}
 	
+	private static ByteBuffer allocate(int nbInt, int size){
+		return ByteBuffer.allocate(  (Integer.BYTES * nbInt) + size);
+	}
 	
 	/** Ask to sc, if the Name is available.
 	 * 
@@ -30,7 +21,7 @@ public class Writters {
 	 */
 	public static void requestName(SocketChannel sc, String name) throws IOException{
 		ByteBuffer buff = UTF8.encode(name);
-		ByteBuffer buff2 = ByteBuffer.allocate(Integer.BYTES*2 + buff.remaining());
+		ByteBuffer buff2 = allocate(2 , buff.remaining());
 		
 		//Code 0 ask for connection
 		buff2.putInt(0);
@@ -39,10 +30,45 @@ public class Writters {
 		buff2.putInt(buff.remaining());
 		
 		buff2.put(buff);
+		buff2.flip();
 		
 		sc.write(buff2);
 		
+	}
+	
+	/**
+	 * Ask a private connection to the client dest.
+	 * @param src
+	 * @param dest
+	 */
+	public static void askPrivateConnection(SocketChannel sc, String src, String dest)throws IOException{
+		ByteBuffer srcBuff = UTF8.encode(src);
+		ByteBuffer destBuff = UTF8.encode(dest);
+
+		ByteBuffer buff = allocate(3,srcBuff.remaining() + destBuff.remaining() );
 		
+		buff.putInt(3).putInt(srcBuff.remaining()).put(srcBuff).putInt(destBuff.remaining()).put(destBuff);
+		
+		buff.flip();
+		
+		sc.write(buff);
+		
+	}
+	
+	/**
+	 * Accept a private connection from the client scr.
+	 * @param sc
+	 * @param src
+	 */
+	public static void acceptPrivateConnection(SocketChannel sc,String src) throws IOException{
+		ByteBuffer srcBuff = UTF8.encode(src);
+		ByteBuffer adressBuff = UTF8.encode(sc.getLocalAddress().toString());
+		ByteBuffer buff = allocate(4,srcBuff.remaining() + adressBuff.remaining());
+		
+		buff.putInt(5).putInt(srcBuff.remaining()).put(srcBuff).putInt(adressBuff.remaining()).put(adressBuff);
+		buff.flip();
+		
+		sc.write(buff);
 	}
 	
 	/**
