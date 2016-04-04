6b78898e997ffc090db7013b4d5501916486f3ef
thneang <thneang@gmail.com>
2016-04-04 21:22:49 +0200

    message avec long

--- BANANA/src/fr/upem/net/logger/Loggers.java
+++ BANANA/src/fr/upem/net/logger/Loggers.java
@@ -35,7 +35,7 @@ public class Loggers {
 		case 2:
 		case 8:
 		case 12:
-		case 13 : System.out.println(sb);
+		case 13 : System.out.println(sb);return;
 		case 3:
 		case 15://Message Client to client and server to client only
 			size = buff.getInt();
