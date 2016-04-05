eaf0839aaa9e21a9a0ffaf3f2849c7b5fc947aea
thneang <thneang@gmail.com>
2016-04-05 23:35:29 +0200

    cl bug private co corriger

--- BANANA/bin/e.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/usr/bash
-javac fr/upem/net/tcp/*/*.java
-case $1 in
-	1) java fr/upem/net/tcp/server/ServerMultiChatTCPNonBlockingWithQueueGoToMatou3 7777 ;;
-	2) java fr/upem/net/tcp/client/ClientTCPMatou localhost 7777 ;;
-esac
