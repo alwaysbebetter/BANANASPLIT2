efb29470d4f95f8d7044a64e545b3f89c20dd4f8
gabbru <gabriel.bruit@gmail.com>
2016-04-10 18:37:34 +0200

    new 1

--- BANANA/src/fr/upem/net/tcp/server/ServerMultiChatTCPNonBlockingWithQueueGoToMatou3.java
+++ BANANA/src/fr/upem/net/tcp/server/ServerMultiChatTCPNonBlockingWithQueueGoToMatou3.java
@@ -440,6 +440,7 @@ public class ServerMultiChatTCPNonBlockingWithQueueGoToMatou3 {
 				break;
 
 			case REF_CO_PRV_SC:
+				System.out.println("write in bb REF_CO_PRV_SC");
 				// Do nothing
 				break;
 			case ACC_CO_PRV_SC:
@@ -503,6 +504,7 @@ public class ServerMultiChatTCPNonBlockingWithQueueGoToMatou3 {
 				break;
 			case ACC_CO_PRV_CS:
 			case REF_CO_PRV_CS:
+				System.out.println("réponse a REF_CO_PRV_CS");
 				at = map.get(dataPacketRead.getLoginDst());
 				out.flip();
 				at.sc.write(out);
@@ -676,13 +678,16 @@ public class ServerMultiChatTCPNonBlockingWithQueueGoToMatou3 {
 					statusTreatment = StatusTreatment.TYPE_READING;
 					break;
 				case REF_CO_PRV_CS:
-					if ((!dataPacketRead.getLoginSrc().equals(login))
-							|| (id != dataPacketRead.getId())) {
+					
+					if (/*(!dataPacketRead.getLoginSrc().equals(login))
+							||*/ (id != dataPacketRead.getId())) {
+						System.out.println("CLOSE AN REJECET CLIENT "+login);
 						// si il s'agit d'une usurpation d'identité on ferme la
 						// connection
 						// TODO: close
 						CloseAnRejectClient(login);
 					}
+					System.out.println("recepetion de REF_CO_PRV_CS");
 					writePacketToSend(dataPacketRead, TypePacket.REF_CO_PRV_SC,
 							out);
 
